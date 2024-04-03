2ccd6e0a9a52d3bd1788ef13ba640c5c67c31ca5
Pedro Costa <scosta.pedro.manoel@gmail.com>
2024-04-03 20:25:01 -0300

    chore: update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -7,7 +7,6 @@ import tseslint from 'typescript-eslint';
 export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
-  // @ts-expect-error
   prettierConfig,
   {
     rules: {
